5e79ffa60708662e88824509c181b76ed868feec
splashyin <yin.hao@rocketmail.com>
2020-09-15 22:18:32 +1200

    Added cmake

---
+++ practice_01/build/Practice_01Config.h
@@ -0,0 +1,3 @@
+// Configure options and setting for Practice_01
+#define Practice_01_VERSION_MAJOR 1
+#define Practice_01_VERSION_MINOR 0
--- practice_01/main.cpp
+++ practice_01/main.cpp
@@ -1,10 +1,22 @@
 #include "pimpl.h"
+#include "Practice_01Config.h"
 #include <iostream>
 
 int main()
 {
-    Player p1( "Anthony" );
-    p1.getHealth();
+    // Print version info
+    std::cout << "Version " << Practice_01_VERSION_MAJOR << "." 
+              << Practice_01_VERSION_MINOR << std::endl;
+     
+    Player p1("Anthony");
+    Player p2( "Sabrina" );
     p1.setHealth(10);
+    p1.getHealth();
+
+    p2.setHealth(15);
+    p1 = p2;
+    p1.getHealth();
     std::cin.get();
+    
+    return 0;
 }
